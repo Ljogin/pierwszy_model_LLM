6a52110d05ce7805508e2a3c6638456129040f50
luk skywalker <ljogin@gmail.com>
2025-10-04 21:44:06 +0200

    dodanie oryginalnej wersji programu app.py z kursu

--- app.py
+++ app.py
@@ -1,27 +1,31 @@
 import json
 import streamlit as st
-import pandas as pd
-from pycaret.clustering import ClusteringPredictor
-import plotly.express as px
+import pandas as pd  # type: ignore
+from pycaret.clustering import load_model, predict_model  # type: ignore
+import plotly.express as px  # type: ignore
 
-MODEL_NAME = 'welcome_survey_clustering_pipeline_v1.pkl'
+MODEL_NAME = 'welcome_survey_clustering_pipeline_v1'
+
 DATA = 'welcome_survey_simple_v1.csv'
+
 CLUSTER_NAMES_AND_DESCRIPTIONS = 'welcome_survey_cluster_names_and_descriptions_v1.json'
+
 
 @st.cache_data
 def get_model():
-    return ClusteringPredictor.load(MODEL_NAME)
+    return load_model(MODEL_NAME)
 
 @st.cache_data
 def get_cluster_names_and_descriptions():
     with open(CLUSTER_NAMES_AND_DESCRIPTIONS, "r", encoding='utf-8') as f:
-        return json.load(f)
+        return json.loads(f.read())
 
 @st.cache_data
-def get_all_participants(model):
+def get_all_participants():
     all_df = pd.read_csv(DATA, sep=';')
-    all_df["Cluster"] = model.predict(all_df)
-    return all_df
+    df_with_clusters = predict_model(model, data=all_df)
+
+    return df_with_clusters
 
 with st.sidebar:
     st.header("Powiedz nam coś o sobie")
@@ -32,35 +36,65 @@ with st.sidebar:
     fav_place = st.selectbox("Ulubione miejsce", ['Nad wodą', 'W lesie', 'W górach', 'Inne'])
     gender = st.radio("Płeć", ['Mężczyzna', 'Kobieta'])
 
-    person_df = pd.DataFrame([{
-        'age': age,
-        'edu_level': edu_level,
-        'fav_animals': fav_animals,
-        'fav_place': fav_place,
-        'gender': gender,
-    }])
+    person_df = pd.DataFrame([
+        {
+            'age': age,
+            'edu_level': edu_level,
+            'fav_animals': fav_animals,
+            'fav_place': fav_place,
+            'gender': gender,
+        }
+    ])
 
 model = get_model()
-all_df = get_all_participants(model)
+all_df = get_all_participants()
 cluster_names_and_descriptions = get_cluster_names_and_descriptions()
 
-predicted_cluster_id = model.predict(person_df)[0]
-predicted_cluster_data = cluster_names_and_descriptions[str(predicted_cluster_id)]
+predicted_cluster_id = predict_model(model, data=person_df)["Cluster"].values[0]
+predicted_cluster_data = cluster_names_and_descriptions[predicted_cluster_id]
 
 st.header(f"Najbliżej Ci do grupy {predicted_cluster_data['name']}")
 st.markdown(predicted_cluster_data['description'])
-
 same_cluster_df = all_df[all_df["Cluster"] == predicted_cluster_id]
 st.metric("Liczba twoich znajomych", len(same_cluster_df))
 
 st.header("Osoby z grupy")
-for column, title in [
-    ("age", "Rozkład wieku w grupie"),
-    ("edu_level", "Rozkład wykształcenia w grupie"),
-    ("fav_animals", "Rozkład ulubionych zwierząt w grupie"),
-    ("fav_place", "Rozkład ulubionych miejsc w grupie"),
-    ("gender", "Rozkład płci w grupie"),
-]:
-    fig = px.histogram(same_cluster_df, x=column)
-    fig.update_layout(title=title, xaxis_title=column, yaxis_title="Liczba osób")
-    st.plotly_chart(fig)
+fig = px.histogram(same_cluster_df.sort_values("age"), x="age")
+fig.update_layout(
+    title="Rozkład wieku w grupie",
+    xaxis_title="Wiek",
+    yaxis_title="Liczba osób",
+)
+st.plotly_chart(fig)
+
+fig = px.histogram(same_cluster_df, x="edu_level")
+fig.update_layout(
+    title="Rozkład wykształcenia w grupie",
+    xaxis_title="Wykształcenie",
+    yaxis_title="Liczba osób",
+)
+st.plotly_chart(fig)
+
+fig = px.histogram(same_cluster_df, x="fav_animals")
+fig.update_layout(
+    title="Rozkład ulubionych zwierząt w grupie",
+    xaxis_title="Ulubione zwierzęta",
+    yaxis_title="Liczba osób",
+)
+st.plotly_chart(fig)
+
+fig = px.histogram(same_cluster_df, x="fav_place")
+fig.update_layout(
+    title="Rozkład ulubionych miejsc w grupie",
+    xaxis_title="Ulubione miejsce",
+    yaxis_title="Liczba osób",
+)
+st.plotly_chart(fig)
+
+fig = px.histogram(same_cluster_df, x="gender")
+fig.update_layout(
+    title="Rozkład płci w grupie",
+    xaxis_title="Płeć",
+    yaxis_title="Liczba osób",
+)
+st.plotly_chart(fig)
